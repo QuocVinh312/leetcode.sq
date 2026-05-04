3767d9a1f073601fd68fc1017b6d69934478bcd3
Phung Quoc Vinh <91551493+QuocVinh312@users.noreply.github.com>
2026-05-04 14:55:02 +0700

    Create 1161-ProjectEmployeesI

---
+++ https:/github.com/QuocVinh312/leetcode.sq.git/1161-ProjectEmployeesI/1161-ProjectEmployeesI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 5/4/2026, 2:55:02 PM
+# Write your MySQL query statement below
+SELECT p.project_id, ROUND(AVG(experience_years),2) AS average_years
+FROM Project p 
+LEFT JOIN Employee e ON p.employee_id = e.employee_id
+GROUP BY project_id
