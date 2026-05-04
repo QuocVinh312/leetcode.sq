cbcccf2e396d7deed4e9dd1f0369f77e05f4223e
Phung Quoc Vinh <91551493+QuocVinh312@users.noreply.github.com>
2026-05-04 14:54:59 +0700

    Create 1338-QueriesQualityAndPercentage

---
+++ https:/github.com/QuocVinh312/leetcode.sq.git/1338-QueriesQualityAndPercentage/1338-QueriesQualityAndPercentage.sql
@@ -0,0 +1,9 @@
+-- Last updated: 5/4/2026, 2:54:59 PM
+# Write your MySQL query statement below
+SELECT 
+    query_name, 
+    ROUND(SUM(rating/position)/COUNT(query_name),2) AS quality,
+    ROUND(AVG(CASE WHEN rating < 3 THEN 1 ELSE 0 END)*100,2) AS poor_query_percentage
+FROM Queries
+GROUP BY query_name
+
